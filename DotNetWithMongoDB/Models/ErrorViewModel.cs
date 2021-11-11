a0ab6e355ec652def3c287ce660238c33528dfa7
Alejandro Rodriguez <alerd6539@gmail.com>
2021-11-11 09:17:33 -0600

    Agregar archivos de proyecto.

---
+++ DotNetWithMongoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetWithMongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
